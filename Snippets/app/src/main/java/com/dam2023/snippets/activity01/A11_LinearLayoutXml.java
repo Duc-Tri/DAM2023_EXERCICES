2d1047da35ea3cea2de57c5fb71cb18e687b596a
Tri <ductri.vu@gmail.com>
2023-01-31 11:19:50 +0100

    3 buttons in linear layout

---
+++ Snippets/app/src/main/java/com/dam2023/snippets/activity01/A11_LinearLayoutXml.java
@@ -0,0 +1,17 @@
+package com.dam2023.snippets.activity01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam2023.snippets.R;
+
+public class A11_LinearLayoutXml extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a11_linear_layout_xml);
+    }
+
+}
