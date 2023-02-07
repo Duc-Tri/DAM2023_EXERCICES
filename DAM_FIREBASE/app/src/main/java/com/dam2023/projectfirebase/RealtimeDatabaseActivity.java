ba02f21690f518b24252ca3fa55f7a4ea88b39e1
Tri <ductri.vu@gmail.com>
2023-02-07 17:24:17 +0100

    firebase realtime database

---
+++ DAM_FIREBASE/app/src/main/java/com/dam2023/projectfirebase/RealtimeDatabaseActivity.java
@@ -0,0 +1,51 @@
+package com.dam2023.projectfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.dam2023.projectfirebase.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RealtimeDatabaseActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    private Button btnMessage;
+    private EditText etMessage;
+
+    private void initUI() {
+        btnMessage = (Button) findViewById(R.id.btnSend);
+        etMessage = (EditText) findViewById(R.id.etMessage);
+    }
+
+    public void setOnClickListener()
+    {
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue(etMessage.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realtime_database);
+        initUI();
+        setOnClickListener();
+    }
+
+}
+
+/*
+MD5: 9B:7F:75:24:7D:5C:04:3E:07:E2:9E:E7:A6:94:7B:78
+SHA1: 6C:53:EF:97:F6:E9:1E:1D:AC:01:F9:4E:29:30:C7:83:57:4D:CF:0A
+SHA-256: 83:48:3E:49:4E:E9:D2:37:1D:0D:E0:27:A2:48:3A:77:2F:8D:F2:85:52:BB:F5:F3:50:73:0A:96:0D:39:10:07
+Valid until: dimanche 17 septembre 2051
+*/
