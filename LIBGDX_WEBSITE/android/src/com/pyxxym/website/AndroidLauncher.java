1fb722f7560968f849b6715c74a43f6d857fa079
Tri <ductri.vu@gmail.com>
2023-02-20 19:41:44 +0100

    Webgl test

---
+++ LIBGDX_WEBSITE/android/src/com/pyxxym/website/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pyxxym.website;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pyxxym.website.WebsiteLibGDX;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WebsiteLibGDX(), config);
+	}
+}
---
+++ LIBGDX_WEBSITE/desktop/src/com/pyxxym/website/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pyxxym.website;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pyxxym.website.WebsiteLibGDX;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Website");
+		new Lwjgl3Application(new WebsiteLibGDX(), config);
+	}
+}
