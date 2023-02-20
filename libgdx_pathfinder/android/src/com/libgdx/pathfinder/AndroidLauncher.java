cf14554300d36e68bac6da263870c7b17aebb9d6
Duc-Tri <ductri.vu@gmail.com>
2023-02-20 17:02:33 +0100

    first commit for pathfinding

---
+++ libgdx_pathfinder/android/src/com/libgdx/pathfinder/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.libgdx.pathfinder;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.libgdx.pathfinder.PathFinder;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PathFinder(), config);
+	}
+}
---
+++ libgdx_pathfinder/html/src/com/libgdx/pathfinder/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.libgdx.pathfinder.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.libgdx.pathfinder.PathFinder;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PathFinder();
+        }
+}
