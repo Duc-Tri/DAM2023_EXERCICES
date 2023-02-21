e0f0e99c614cb14dc04fb68a9c04fae3413324b8
Duc-Tri <ductri.vu@gmail.com>
2023-02-21 17:03:45 +0100

    start of login

---
+++ DAM_LOGIN/app/src/main/java/com/dam2023/login/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.dam2023.login.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam2023.login.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
