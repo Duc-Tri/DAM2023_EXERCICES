a1f298a20a4cc650c22f5c13933745d83a7aec18
Duc-Tri <ductri.vu@gmail.com>
2023-02-23 15:43:48 +0100

    make DamAppChat running (login is breaking)

--- DAM_LOGIN/app/src/main/java/com/dam2023/login/login/LoginActivity.java
+++ DAM_LOGIN/app/src/main/java/com/dam2023/login/login/LoginActivity.java
@@ -1,16 +1,98 @@
 package com.dam2023.login.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dam2023.login.MainActivity;
 import com.dam2023.login.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText etEmail, etPassword;
+    private String email, password;
+
+
+    private void initUI() {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+    public void btnLoginClick(View view) {
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if (email.equals("")) {
+            etEmail.setError(getString(R.string.enter_email));
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError(getString(R.string.enter_correct_email));
+        } else {
+
+            // AUTH -------------------------------------------------------------------------------
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        // complete => on peut gèrer le cancel et failure dedans AUSSI !
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                                // finit l'activité courante ! plus sécure à cause du password
+                                finish();
+                            } else {
+                                // gestion de failure (autre possibilité que addOnFailureListener)
+                                Toast.makeText(LoginActivity.this, "Login failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, "Login failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        initUI();
+        etPassword.setOnEditorActionListener(editorActionListener);
     }
+
+    private TextView.OnEditorActionListener editorActionListener=new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+            switch (actionId)
+            {
+                //............
+                case EditorInfo.IME_ACTION_DONE:
+                    btnLoginClick(textView);
+            }
+
+            return false;
+        }
+    };
+
 }
